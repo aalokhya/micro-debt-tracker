8650dad3d3514c38c66eef377cab7eee26bd9841
Aalokhya Karlapati <108568513+aalokhya@users.noreply.github.com>
2025-05-28 11:28:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -188,4 +188,6 @@ def edit_expense(expense_id):
         return render_template('edit_expense.html', expense=expense, users=users, involved_user_ids=involved_user_ids)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
